e2c1a619ed2f2712c921d4cb1615512319e8742a
Rodolfo Rivera <rorivera0621@gmail.com>
2023-10-01 14:52:14 -0700

    added php

--- the-magic/main.php
+++ the-magic/main.php
@@ -6,7 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.cdnfonts.com/css/gondrin" rel="stylesheet"> 
     <link rel="stylesheet" href="index.css">       
-    <script type="text/javascript" src="index.js"></script>
     <title>440 Project</title>
 </head>
 <body>
@@ -76,4 +75,55 @@
     </div>
 
 </body>
+<script>
+    var theLogin;
+var theRegister;
+
+function init() {
+  //div stuff
+  theLogin = document.getElementById("login-box");
+  theRegister = document.getElementById("register-box");
+
+  //header stuff
+  loginHeader = document.getElementById("login-header");
+  registerHeader = document.getElementById("register-header");
+  loginHeader.addEventListener("click", Login);
+  registerHeader.addEventListener("click", SignUp);
+
+  //theLogin.addEventListener("click", Login);
+  //theRegister.addEventListener("click", SignUp);
+
+  makeLoginVisible();
+  makeRegisterInvisible();
+}
+
+function makeRegisterInvisible() {
+  theRegister.style.display = "none";
+}
+
+function makeRegisterVisible() {
+  theRegister.style.display = "flex";
+}
+
+function makeLoginInvisible() {
+  theLogin.style.display = "none";
+}
+
+function makeLoginVisible() {
+  theLogin.style.display = "flex";
+}
+
+function SignUp() {
+  makeLoginInvisible();
+  makeRegisterVisible();
+}
+
+function Login() {
+  makeRegisterInvisible();
+  makeLoginVisible();
+}
+
+window.addEventListener("load", init);
+</script>
 </html>
+
