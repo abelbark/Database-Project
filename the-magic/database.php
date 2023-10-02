0b80537dd4f60929ff07c62e3be3fa48fac32524
Rodolfo Rivera <rorivera0621@gmail.com>
2023-10-02 15:55:21 -0700

    db connected

--- the-magic/database.php
+++ the-magic/database.php
@@ -3,10 +3,10 @@
     error_reporting(E_ALL);
     ini_set('display_errors', 1);
 
-    $host = "localhost";
+    $host = "my440cluster.cevb6hoe2jwx.us-east-2.rds.amazonaws.com";
     $dbname = "440Group";
-    $username = "root";
-    $password = "mysql";
+    $username = "admin";
+    $password = "somepassword";
 
     $mysqli = new mysqli($host, $username, $password, $dbname);
     $mysqli->autocommit(true);
