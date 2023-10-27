5504f91b68c2e8c6f49f32a33be6472135e5dcf8
abelbark <106646800+abelbark@users.noreply.github.com>
2023-10-26 18:04:59 -0700

    Update arrival.php

--- the-magic/arrival.php
+++ the-magic/arrival.php
@@ -1,16 +1,8 @@
 
 <?php
-    session_start();
 
-    if(isset($_SESSION["first-name"])){
+    include "createItem.php";
 
-        $firstName = $_SESSION["first-name"];
-
-    } else {
-
-        header('Location: main.php');
-        exit();
-    }
 ?>
 
 <!DOCTYPE html>
@@ -19,21 +11,90 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="index.css">
+    <link href="https://fonts.cdnfonts.com/css/gondrin" rel="stylesheet">
+    <link rel="stylesheet" href="item.css">
     <title>Arrival</title>
+
+    <script>
+
+        var theItem;
+        var itemPrice;
+
+        function init(){
+
+            theItem = document.getElementById("item-box");
+            itemPrice = document.getElementById("item-price");
+
+            itemPrice.value = "0.00";
+
+            itemPrice.addEventListener("input", formatMoney);
+
+        }
+
+        function makeItemBoxVisisble(){
+            theItem.style.display = "flex";
+        }
+
+
+        function formatMoney(){
+
+            var input = itemPrice.value;
+            input = input.replace(/[^\d.]/g, "");
+            var parts = input.split(".");
+            var decimalPart = parts[1];
+
+            if(!decimalPart || decimalpart.length != 2){
+                alert("Format must be 'x.xx'");
+                itemPrice.value = "0.00";
+                event.preventDefault();
+            }
+
+            
+        }
+
+
+        window.addEventListener("load", init);
+    </script>
+
+
 </head>
 <body>
 
-    <div class="welcome-header">
-        <h1>Welcome <?php echo $firstName; ?></h1>
-    </div>
+    <div class="arrival-container">
+        
 
-    
-    <!--check if the user is logged in-->
- 
-    <div class="crowd-container">
-        <img src="https://media1.giphy.com/media/35AZzA7QYVyxhvnGU6/giphy.gif?cid=6c09b952o0fek76twqaqi64msv0nwuumuim6nm4an7urngsu&ep=v1_internal_gif_by_id&rid=giphy.gif&ct=s"
-         id="crowd" name="crowd-cheer">
+        <div class="header-container">
+            <h3 id="create-header">Item</h3>
+        </div>
+
+        <div class="input-form">
+            <!-- SignUp form-->
+            <div id="item-box">
+                <form action="createItem.php" method="post">
+                    <div class="item-input">
+                        <input type="text" 
+                            id="item-name" 
+                            placeholder="Item Name" 
+                            name="item-name"><br>
+                        <input type="text" 
+                            id="item-category" 
+                            placeholder="Item Category" 
+                            name="item-category"><br>
+                         <input type="text" 
+                            id="item-price"
+                            placeholder="0.00" 
+                            name="item-price"><br>
+                        <textarea
+                            id="item-description"
+                            placeholder="Item description"
+                            name="item-description"></textarea><br>
+
+                        <input type="submit" id="create" value="Create">
+                        <p><a href="arrival.php?logout=1">Log Out</a></p>
+                    </div>
+                </form>
+            </div>
+        </div>
     </div>
     
 </body>
