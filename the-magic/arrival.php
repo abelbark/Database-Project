6057dace69e6c0c7e9f98682a93b52954fc5892f
Rodolfo Rivera <rorivera0621@gmail.com>
2023-11-01 12:52:42 -0700

    added success pop up

--- the-magic/arrival.php
+++ the-magic/arrival.php
@@ -1,7 +1,68 @@
 
 <?php
+    $posted = false;
+     error_reporting(E_ALL);
+    ini_set('display_errors', 1);
 
-    include "createItem.php";
+    session_start();
+    
+    if(isset($_SESSION["first-name"])){
+
+        $firstName = $_SESSION["first-name"];
+
+        if($_SERVER["REQUEST_METHOD"] === "POST"){
+            if(empty($_POST['item-name']) || empty($_POST['item-category']) ||
+               empty($_POST['item-price']) || empty($_POST['item-description'])){
+    
+                   die("fill in the fields!!!");
+    
+            } else {
+    
+                $mysqli = require "database.php";
+    
+                $sql = "INSERT INTO item(Title, Category, Price, Description)
+                        VALUES (?, ?, ?, ?)";
+    
+                $stmt = $mysqli->stmt_init();
+    
+                if(!$stmt->prepare($sql)){
+                    die("SQL error: " . $mysqli->error);
+                }
+    
+                $price = floatval($_POST["item-price"]);
+    
+                $stmt->bind_param("ssds", 
+                $_POST["item-name"],
+                $_POST["item-category"],
+                $price,
+                $_POST["item-description"]);
+    
+                if($stmt->execute()){
+                   $posted = true;
+                } else {
+    
+                    die($mysqli->error . " " . $mysqli->errno);
+    
+                }
+    
+    
+    
+            }
+        }
+
+    } else {
+
+        header('Location: main.php');
+        exit();
+    }
+
+    //logs out the user from the page
+    if(isset($_GET["logout"])){
+
+        session_destroy();
+        header("Location: main.php");
+        exit;
+    }
 
 ?>
 
@@ -59,7 +120,12 @@
 
 </head>
 <body>
-
+    <?php
+    if( $posted ) {
+        echo "<script type='text/javascript'>alert('submitted successfully!')</script>";
+        // $posted = false;
+    }
+    ?>
     <div class="arrival-container">
         
 
@@ -70,7 +136,7 @@
         <div class="input-form">
             <!-- SignUp form-->
             <div id="item-box">
-                <form action="createItem.php" method="post">
+                <form action="arrival.php" method="post">
                     <div class="item-input">
                         <input type="text" 
                             id="item-name" 
