59a86af260c7d0283ab8aee558a3ee9f112228d6
abelbark <106646800+abelbark@users.noreply.github.com>
2023-11-25 18:06:19 -0800

    Update arrival.php

--- the-magic/arrival.php
+++ the-magic/arrival.php
@@ -1,6 +1,5 @@
 <?php
     $posted = false;
-    $postsExceeded = false;
     $redirectBack = false;
     error_reporting(E_ALL);
     ini_set('display_errors', 1);
@@ -32,68 +31,65 @@
             // Display a pop-up message after initialization
             echo "<script type='text/javascript'>alert('Initialized!');
                     window.location.href = 'arrival.php'</script>";
+            exit;  // Stop execution after initialization
         }
 
         // Get the current date in the correct format
         $date = date('Y-m-d');
-
-        // Check the number of posts for the current user on the current date
-        $sqlCheckLimit = "SELECT COUNT(*) AS post_count FROM item
-            WHERE username = ? AND DATE_FORMAT(NOW(), '%Y-%m-%d') = ?"; // Use NOW() to get the current date
-
-        $stmtCheckLimit = $mysqli->prepare($sqlCheckLimit);
-        $stmtCheckLimit->bind_param("ss", $userName, $date);
-        $stmtCheckLimit->execute();
-        $resultCheckLimit = $stmtCheckLimit->get_result();
-        $row = $resultCheckLimit->fetch_assoc();
-        $postCountToday = $row["post_count"];
-
-        $stmtCheckLimit->close();
 
         if ($_SERVER["REQUEST_METHOD"] === "POST") {
             if (empty($_POST['item-name']) || empty($_POST['item-category']) ||
                 empty($_POST['item-price']) || empty($_POST['item-description'])) {
                 die("Fill in all the fields!");
             } else {
+                // Check the number of posts for the current user on the current date
+                $sqlCheckLimit = "SELECT COUNT(*) AS post_count FROM item
+                    WHERE username = ? AND DATE_FORMAT(NOW(), '%Y-%m-%d') = ?"; // Use NOW() to get the current date
+
+                $stmtCheckLimit = $mysqli->prepare($sqlCheckLimit);
+                $stmtCheckLimit->bind_param("ss", $userName, $date);
+                $stmtCheckLimit->execute();
+                $resultCheckLimit = $stmtCheckLimit->get_result();
+                $row = $resultCheckLimit->fetch_assoc();
+                $postCountToday = $row["post_count"];
+
+                $stmtCheckLimit->close();
+
                 if ($postCountToday >= 3) {
-                    $postsExceeded = true;
                     $redirectBack = true;
+                    echo "<script type= 'text/javascript'>
+                            alert('You Hit Your Daily Limit!');
+                            window.location.href = 'arrival.php';
+                          </script>";
+                    exit;  // Stop execution if daily limit is exceeded
+                }
+
+                // Prepare the INSERT statement
+                $sql = "INSERT INTO item(Title, Category, Price, Description, username)
+                    VALUES (?, ?, ?, ?, ?)";
+
+                $stmt = $mysqli->prepare($sql);
+
+                if (!$stmt) {
+                    die("SQL error: " . $mysqli->error);
+                }
+
+                $price = floatval($_POST["item-price"]);
+
+                $stmt->bind_param("ssdss", 
+                    $_POST["item-name"],
+                    $_POST["item-category"],
+                    $price,
+                    $_POST["item-description"],
+                    $userName
+                );
+
+                if ($stmt->execute()) {
+                    $posted = true;
                 } else {
-                    // Prepare the INSERT statement
-                    $sql = "INSERT INTO item(Title, Category, Price, Description, username)
-                        VALUES (?, ?, ?, ?, ?)";
-
-                    $stmt = $mysqli->prepare($sql);
-
-                    if (!$stmt) {
-                        die("SQL error: " . $mysqli->error);
-                    }
-
-                    $price = floatval($_POST["item-price"]);
-
-                    $stmt->bind_param("ssdss", 
-                        $_POST["item-name"],
-                        $_POST["item-category"],
-                        $price,
-                        $_POST["item-description"],
-                        $userName
-                    );
-
-                    if ($stmt->execute()) {
-                        $posted = true;
-                    } else {
-                        die($mysqli->error . " " . $mysqli->errno);
-                    }
+                    die($mysqli->error . " " . $mysqli->errno);
                 }
             }
-        }
-
-        if ($redirectBack) {
-            echo "<script type= 'text/javascript'>
-                alert('You Hit Your Daily Limit!');
-                window.location.href = 'arrival.php';
-              </script>";
-            exit;
         }
     } else {
         header('Location: main.php');
@@ -118,7 +114,6 @@
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <link rel="stylesheet" href="item.css">
     <title>Arrival</title>
-
 </head>
 <body>
     <?php
