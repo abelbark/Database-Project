210519f08a2a89b56748c96a0f0072dbc7b2a6f9
abelbark <106646800+abelbark@users.noreply.github.com>
2023-11-25 21:53:47 -0800

    Create number6.php

---
+++ the-magic/phase-3/number6.php
@@ -0,0 +1,42 @@
+
+<?php
+    $databasePath = __DIR__ . '/../database.php';
+    $mysqli = require $databasePath;
+
+    $sql = "SELECT DISTINCT u.username
+            FROM user u   
+            WHERE NOT EXISTS (
+                SELECT 1
+                from reviews r
+                join item i on r.itemID = i.itemID
+                where r.userName = u.username and r.rating = 'excellent'
+                having count(*) >= 3
+            )";
+    $result = $mysqli->query($sql);
+
+    if($result){
+        $users = $result->fetch_all(MYSQLI_ASSOC);
+    } else {
+        die($mysqli->error);
+    }
+
+    $mysqli->close();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Number 6</title>
+</head>
+<body>
+    <h1>6) Users who never posted any "excellent" items</h1>
+    <ul>
+        <?php  foreach($users as $user): ?>
+            <li><?= $user['username']; ?></li>
+        <?php  endforeach; ?>
+    </ul>
+</body>
+</html>
